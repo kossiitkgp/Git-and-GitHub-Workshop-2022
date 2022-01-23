35f79994bb41330bf2e9761665e7692b84fe87c4
Aritra Chakraborty <aritracodm2002@kgpian.iitkgp.ac.in>
2022-01-24 02:29:56 +0530

    Added Subtraction functions in all the three sample codes

--- code_samples/sample.c
+++ code_samples/sample.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <math.h>
+
+int subtract_two_numbers(int a,int b) {
+    int diff;
+    diff = (int)fabs((double)(a - b));
+    return diff;
+}
 
 int add_two_numbers(int a,int b) {
     int sum;
@@ -8,6 +15,7 @@ int add_two_numbers(int a,int b) {
 
 
 int main() {
-    printf("%d", add_two_numbers(5,2)); // expected result 7
+    printf("The sum of 2 and 5 is: %d\n", add_two_numbers(2,5)); // expected result 7
+    printf("The difference of 2 and 5 is: %d", subtract_two_numbers(2,5)); // expected result 3
 }
 //just a comment added 
--- code_samples/sample.js
+++ code_samples/sample.js
@@ -1,6 +1,17 @@
 function add_two_numbers(a,b) {
     const sum = a + b
-    console.log(sum)
+    console.log("The sum of the two numbers is",sum)
+}
+
+function subtract_two_numbers(a,b) {
+    var diff = 0
+    if(a>b){
+        diff=a-b
+    }else{
+        diff=b-a
+    }
+    console.log("The difference of the two numbers is",diff)
 }
 
 add_two_numbers(5,2) // expected result 7
+subtract_two_numbers(2,5) // expected result 3
