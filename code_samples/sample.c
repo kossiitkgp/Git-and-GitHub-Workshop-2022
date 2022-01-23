8a96e1cb78d22134e237c8f7c8284fb45351d48f
Aritra Chakraborty <aritracodm2002@kgpian.iitkgp.ac.in>
2022-01-24 01:13:42 +0530

    Fixed issues in sample.c

--- code_samples/sample.c
+++ code_samples/sample.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int add_two_numbers(a,b) {
+int add_two_numbers(int a,int b) {
     int sum;
-    sum = a - b;
-    printf("%d", sum);
+    sum = a + b;
+    return sum;
 }
 
 
 int main() {
-    add_two_numbers(5,2); // expected result 7
+    printf("%d", add_two_numbers(5,2)); // expected result 7
 }
 //just a comment added 
