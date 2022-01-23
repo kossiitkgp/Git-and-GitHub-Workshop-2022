a3b0e70a305583b42778be0033a22b9fb92eaccd
Karthikeya Y <90907535+YK553@users.noreply.github.com>
2022-01-23 16:22:16 +0530

    Fixed bugs in sample.c

--- code_samples/sample.c
+++ code_samples/sample.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int add_two_numbers(a,b) {
+int add_two_numbers(int a,int b) {
     int sum;
-    sum = a - b;
-    printf("%d", sum);
+    sum = a + b;
+    return sum;
 }
 
 
 int main() {
-    add_two_numbers(5,2); // expected result 7
+    int x;
+    x = add_two_numbers(5,2); // expected result 7
+    printf("The sum of 5 and 7 is: %d",x);
+    return 0;
 }
